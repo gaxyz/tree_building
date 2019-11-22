4d2b4207f9c1fdae41fccfa7745dc01e4f8e779c
Gaston Rijo <grijo@tonatiuh.ipmont.lan>
2019-11-22 15:23:58 -0300

    Working renaming

--- extras/rename_fastas.py
+++ extras/rename_fastas.py
@@ -11,18 +11,19 @@ outfile = sys.argv[2]
 
 prot_dict ={}
 
-for fasta in glob.glob(fasta_dir + "/" + "*.aas " ):
+for fasta in glob.glob(fasta_dir + "/" + "*.aas" ):
     for seq in SeqIO.parse( fasta, "fasta"):
         
 
         clname = fasta.split("/")[-1].split(".")[0]
-        newname = seq.id + "_" clname
+        newname = seq.id + "_" + clname
 
         prot_dict[newname] = str( seq.seq )
 
-with open( outfile , 'r' ) as handle:
+with open( outfile , 'w' ) as handle:
 
     for name in prot_dict:
+        print(name)
         handle.write(  ">" + name + "\n"   )
         handle.write( prot_dict[name] + "\n" )
 
